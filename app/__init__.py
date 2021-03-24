0cfd68b9a5f8f58050d70c8c61c502930244ab2a
Yachin Chang <49171617+yacheese@users.noreply.github.com>
2021-03-23 20:17:49 -0700

    Changes

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 from flask import request
 
 app = Flask(__name__)
@@ -17,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail= Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
